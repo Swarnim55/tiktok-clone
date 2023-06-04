3715bcf2cc1ded88a8f2fad0664d877d8625ccc8
Swarnim Paudyal <swarnimpaudyal55@gmail.com>
2023-06-04 17:52:32 +0545

    added video section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,42 @@
 import { Inter } from 'next/font/google';
+import type { NextPage } from 'next';
+import axios from 'axios';
+
+import { Video } from '@/types';
+import VideoCard from '@/components/VideoCard';
+import NoResults from '@/components/NoResults';
+
+interface IProps {
+  videos: Video[];
+}
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Testing Tailwind</h1>
+      <div className="flex flex-col gap-10 videos h-full">
+        {videos.length ? (
+          videos.map((video: Video) => (
+            <VideoCard post={video} key={video._id} />
+          ))
+        ) : (
+          <NoResults text={'No Videos'} />
+        )}
+      </div>
     </>
   );
-}
+};
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+
+export default Home;
