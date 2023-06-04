9fd1b1c6e9171b419ea557daaafd784b1f2b991a
Swarnim Paudyal <swarnimpaudyal55@gmail.com>
2023-06-04 12:12:34 +0545

    added sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = ({ Component, pageProps }: AppProps) => {
   //making SSR option available for smooth rendering
@@ -16,18 +17,21 @@ const App = ({ Component, pageProps }: AppProps) => {
   //if SSR we don't want to render our components
 
   if (isSSR) return null;
+  /* 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 */
 
   return (
     <>
-      <Navbar />
-      <div className="flex gap-6 md:gap-20">
-        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          <Sidebar />
+      <GoogleOAuthProvider clientId="1018479206991-q6vuhto5uf5bfirfdvgcv8vrf3r76nq2.apps.googleusercontent.com">
+        <Navbar />
+        <div className="flex gap-6 md:gap-20">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1 ">
+            <Component {...pageProps} />
+          </div>
         </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1 ">
-          <Component {...pageProps} />
-        </div>
-      </div>
+      </GoogleOAuthProvider>
     </>
   );
 };
